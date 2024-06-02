58388ad274f6d3c5959bf221509217e39d6c5d78
trentraynard <trentersr@gmail.com>
2024-06-02 00:17:40 -0700

    adding headers, user agent, and delay on curl

--- src/scraper.php
+++ src/scraper.php
@@ -1,11 +1,48 @@
 <?php
 //curl to get html
 function get_html($url) {
+    // Set up curl 
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_HEADER, ['accept: application/json']);
-    $html = curl_exec($curl);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    $user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"
+    ];
+    // Get random user agent
+    $user_agent = $user_agents[rand(0,count($user_agents)-1)];
+    curl_setopt($curl, CURLOPT_USERAGENT, $user_agent);
+    // Set up Referer
+    curl_setopt($curl, CURLOPT_REFERER, 'https://www.google.com');
+    // Set up mimic headers
+    curl_setopt($curl, CURLOPT_HTTPHEADER, [
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language: en-US,en;q=0.5',
+        'Connection: keep-alive',
+        'Cache-Control: no-cache'
+    ]);
+    // Set up delay
+    curl_setopt($curl, CURLOPT_PROGRESSFUNCTION, function() {
+        usleep(100000);
+        return 0;
+    });
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
     curl_close($curl);
-    return $html;
+    // Log response and return result
+    if ($err) {
+        echo 'cURL error: ' . $err;
+    } else {
+        return $response;
+    }
+    
 }
 
